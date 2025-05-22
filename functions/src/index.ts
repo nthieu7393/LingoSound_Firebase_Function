545a6fce3fd743872583e89f43f658d490669b68
Hieu Nguyen <nt.hieu7393@gmail.com>
2025-05-22 09:12:29 +0700

    cm

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,16 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const redirectToGame = onRequest((req, res) => {
+    const userAgent = req.get("User-Agent") || "";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Simple validation: allow only app clients (adjust logic)
+    if (!userAgent.includes("MyFlutterApp")) {
+        res.status(403).send("Forbidden");
+        return;
+    }
+
+    const targetUrl = "https://firebasestorage.googleapis.com/v0/b/lingosound-fe5f3.firebasestorage.app/o/lessons%2Fcolors%2Fred%2FThe%20color%20red.mp4?alt=media&token=ae6f3575-0d9a-47f9-b7a0-9654379c3770";
+    logger.info(`Redirecting to: ${targetUrl}`);
+    res.redirect(targetUrl);
+});
