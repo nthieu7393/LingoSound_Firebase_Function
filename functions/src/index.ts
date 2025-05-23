1e6361f7a050171ffd3274ede583f2dfc363c896
hieu nguyen <nt.hieu7393@gmail.com>
2025-05-23 23:31:47 +0700

    cm

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,9 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions/v2/https";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
 
 export const redirectToGame = onRequest((req, res) => {
   const userAgent = req.get("User-Agent") || "";
@@ -12,4 +16,37 @@ export const redirectToGame = onRequest((req, res) => {
   const targetUrl = "https://firebasestorage.googleapis.com/v0/b/lingosound-fe5f3.firebasestorage.app/o/lessons%2Fcolors%2Fred%2FThe%20color%20red.mp4?alt=media&token=5c1dac57-d153-4850-8e2e-c5989d932f3d";
   logger.info(`Redirecting to: ${targetUrl}`);
   res.redirect(targetUrl);
+});
+
+export const getSignedLessonUrl = functions.onRequest(async (req, res) => {
+  const idToken = req.headers.authorization?.split("Bearer ")[1];
+
+  if (!idToken) {
+    res.status(401).send("Missing ID token");
+    return;
+  }
+
+  try {
+    await admin.auth().verifyIdToken(idToken);
+    // const uid = decoded.uid;
+
+    // You can also check roles, email, etc. here if needed
+
+    const filePath = "lessons/colors/red/The color red.mp4";
+
+    const [signedUrl] = await admin
+      .storage()
+      .bucket()
+      .file(filePath)
+      .getSignedUrl({
+        action: "read",
+        expires: Date.now() + 5 * 60 * 1000, // valid for 5 minutes
+      });
+
+    res.status(200).send({ url: signedUrl });
+
+  } catch (err) {
+    console.error("Auth sfailed:", err);
+    res.status(403).send(err);
+  }
 });
